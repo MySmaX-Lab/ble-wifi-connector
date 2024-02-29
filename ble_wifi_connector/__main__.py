350f6980e5e1f7e9831ff0ae75fdaa3afa012b44
thsvkd <thsxogud1123@gmail.com>
2024-02-29 14:55:38 +0900

    Feature: Add BLE WiFi Connector
    Signed-off-by: thsvkd <thsxogud1123@gmail.com>

---
+++ ble_wifi_connector/__main__.py
@@ -0,0 +1,100 @@
+from enum import Enum, auto
+import asyncio
+
+from big_thing_py.utils import *
+from ble_wifi_connector.ble_advertiser import BLEAdvertiser, BLEErrorCode
+from ble_wifi_connector.wifi_manager import WiFiManager
+
+EVENT_LOOP_TIME_OUT = 0.01
+CONNECT_RETRY = 3
+
+
+class BLEWiFiConnectorState(Enum):
+    RESET = auto()
+    BLE_ADVERTISE = auto()
+    NETWORK_SETUP = auto()
+    NETWORK_CONNECTED = auto()
+    NETWORK_LOST = auto()
+    NETWORK_RECONNECTED = auto()
+    SHUTDOWN = auto()
+
+
+async def main_event_loop():
+    connect_try = CONNECT_RETRY
+    state = BLEWiFiConnectorState.RESET
+    ble_advertiser = BLEAdvertiser()
+    wifi_manager = WiFiManager()
+
+    ssid = ''
+    pw = ''
+
+    while True:
+        try:
+            await asyncio.sleep(EVENT_LOOP_TIME_OUT)
+
+            if state == BLEWiFiConnectorState.RESET:
+                state = BLEWiFiConnectorState.BLE_ADVERTISE
+            elif state == BLEWiFiConnectorState.BLE_ADVERTISE:
+                # BLE Advertise
+                await ble_advertiser.start()
+                if not await ble_advertiser.is_advertising():
+                    MXLOG_DEBUG(f'BLE Advertiser start failed...', 'red')
+                    await ble_advertiser.stop()
+                    state = BLEWiFiConnectorState.RESET
+                    continue
+
+                # Save WiFi, Broker info
+                MXLOG_DEBUG(f'Wait for WiFi credentials from BLE...', 'yellow')
+                wifi_credential = await ble_advertiser.wait_until_wifi_credentials_set(timeout=None)
+                ssid = wifi_credential[0]
+                pw = wifi_credential[1]
+                error = wifi_credential[2]
+
+                if error != BLEErrorCode.NO_ERROR:
+                    MXLOG_DEBUG(f'Something getting wrong while BLE setup! error code: {error}', 'red')
+                    await ble_advertiser.stop()
+                    state = BLEWiFiConnectorState.RESET
+                    continue
+
+                await ble_advertiser.stop()
+                state = BLEWiFiConnectorState.NETWORK_SETUP
+            elif state == BLEWiFiConnectorState.NETWORK_SETUP:
+                # WiFi Connect
+                wifi_manager.set_wifi(ssid=ssid, password=pw)
+                await wifi_manager.connect()
+                if wifi_manager.check_connection():
+                    MXLOG_DEBUG(f'WiFi connection success. SSID: {wifi_manager.get_connected_wifi_ssid()}', 'green')
+                    state = BLEWiFiConnectorState.NETWORK_CONNECTED
+                else:
+                    if connect_try > 0:
+                        MXLOG_DEBUG(f'Connect to SSID {wifi_manager.ssid} failed... (try: {connect_try})', 'yellow')
+                        connect_try -= 1
+                        state = BLEWiFiConnectorState.NETWORK_SETUP
+                    else:
+                        MXLOG_DEBUG(f'WiFi connection failed... Go back to BLE setup.', 'red')
+                        connect_try = CONNECT_RETRY
+                        state = BLEWiFiConnectorState.RESET
+            elif state == BLEWiFiConnectorState.NETWORK_CONNECTED:
+                if not wifi_manager.check_connection():
+                    MXLOG_DEBUG(f'WiFi connection lost...', 'yellow')
+                    state = BLEWiFiConnectorState.NETWORK_LOST
+            elif state == BLEWiFiConnectorState.NETWORK_LOST:
+                if not ssid == '' and not pw == '':
+                    state = BLEWiFiConnectorState.NETWORK_SETUP
+                else:
+                    state = BLEWiFiConnectorState.RESET
+            elif state == BLEWiFiConnectorState.SHUTDOWN:
+                if await ble_advertiser.is_advertising():
+                    await ble_advertiser.stop()
+
+                return 0
+        except asyncio.CancelledError:
+            MXLOG_DEBUG("main_event_loop cancelled")
+            state = BLEWiFiConnectorState.SHUTDOWN
+        except KeyboardInterrupt:
+            MXLOG_DEBUG("main_event_loop cancelled")
+            state = BLEWiFiConnectorState.SHUTDOWN
+
+
+if __name__ == "__main__":
+    asyncio.run(main_event_loop())
---
+++ ble_wifi_connector/ble_advertiser.py
@@ -0,0 +1,223 @@
+import asyncio
+from bless import BlessServer, BlessGATTCharacteristic, GATTCharacteristicProperties, GATTAttributePermissions
+
+from big_thing_py.utils import *
+
+
+class BLEErrorCode(Enum):
+    NO_ERROR = 0
+    FAIL = -1
+    WIFI_PASSWORD_ERROR = -2
+    WIFI_CONNECT_TIMEOUT = -3
+    ALREADY_CONNECTED = -4
+    WIFI_NOT_FOUND = -5
+    WIFI_CREDENTIAL_NOT_SET = -6
+    BROKER_NOT_SET = -7
+
+
+class Characteristic:
+    def __init__(self, uuid: str, properties: GATTCharacteristicProperties, permissions: GATTAttributePermissions, value: bytearray = None):
+        self.uuid = uuid
+        self.properties = properties
+        self.permissions = permissions
+        self.value = value
+
+
+class Service:
+    def __init__(self, uuid: str, characteristics: List[Characteristic]):
+        self.uuid = uuid
+        self.characteristics = characteristics
+
+
+class HubWifiService(Service):
+    UUID = '540F0000-0000-0000-0000-000000000000'
+
+    class SetWifiSSIDCharacteristic(Characteristic):
+        def __init__(self):
+            super().__init__(
+                uuid='540F0001-0000-0000-0000-000000000000',
+                properties=GATTCharacteristicProperties.write,
+                permissions=GATTAttributePermissions.writeable,
+            )
+
+    class SetWifiPWCharacteristic(Characteristic):
+        def __init__(self):
+            super().__init__(
+                uuid='540F0002-0000-0000-0000-000000000000',
+                properties=GATTCharacteristicProperties.write,
+                permissions=GATTAttributePermissions.writeable,
+            )
+
+    class ConnectWifiCharacteristic(Characteristic):
+        def __init__(self):
+            super().__init__(
+                uuid='540F0003-0000-0000-0000-000000000000',
+                properties=GATTCharacteristicProperties.write,
+                permissions=GATTAttributePermissions.writeable,
+            )
+
+    class HubIDCharacteristic(Characteristic):
+        def __init__(self):
+            super().__init__(
+                uuid='540F0004-0000-0000-0000-000000000000',
+                properties=GATTCharacteristicProperties.read,
+                permissions=GATTAttributePermissions.readable,
+                value=self.get_middleware_identifier('/usr/local/myssix/middleware/middleware.cfg').encode(),
+            )
+
+        def get_middleware_identifier(self, config_path) -> str:
+            with open(config_path, 'r') as file:
+                for line in file:
+                    stripped_line: str = line.split('//')[0].strip()
+                    if stripped_line.startswith('middleware_identifier'):
+                        return f'''{stripped_line.split('=')[1].strip().strip('"')}_{getmac.get_mac_address().replace(':', '').upper()}'''
+
+    class ErrorCodeCharacteristic(Characteristic):
+        def __init__(self):
+            super().__init__(
+                uuid='540F0005-0000-0000-0000-000000000000',
+                properties=GATTCharacteristicProperties.read,
+                permissions=GATTAttributePermissions.readable,
+            )
+
+    def __init__(self):
+        characteristics = [
+            self.SetWifiSSIDCharacteristic(),
+            self.SetWifiPWCharacteristic(),
+            self.ConnectWifiCharacteristic(),
+            self.HubIDCharacteristic(),
+            self.ErrorCodeCharacteristic(),
+        ]
+        super().__init__(HubWifiService.UUID, characteristics)
+
+
+class BLEAdvertiser:
+    def __init__(self, server_name: str = 'MySSIX BLE SERVER') -> None:
+        self._server_name = server_name
+        self._server: BlessServer = None
+        self._trigger = asyncio.Event()
+
+    def _read_request(self, characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
+        MXLOG_DEBUG(f'Reading {characteristic.value}')
+        return characteristic.value
+
+    def _write_request(self, characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
+        try:
+            characteristic.value = value
+            uuid = characteristic.uuid.upper()
+            if uuid == HubWifiService.SetWifiSSIDCharacteristic().uuid:
+                MXLOG_DEBUG(f'WiFi SSID set: {self._server.get_characteristic(HubWifiService.SetWifiSSIDCharacteristic().uuid).value}')
+            elif uuid == HubWifiService.SetWifiPWCharacteristic().uuid:
+                MXLOG_DEBUG(f'WiFi PW set: {self._server.get_characteristic(HubWifiService.SetWifiPWCharacteristic().uuid).value}')
+            elif uuid == HubWifiService.ConnectWifiCharacteristic().uuid:
+                ssid = self._server.get_characteristic(HubWifiService.SetWifiSSIDCharacteristic().uuid).value
+                pw = self._server.get_characteristic(HubWifiService.SetWifiPWCharacteristic().uuid).value
+                if ssid is None or pw is None:
+                    MXLOG_DEBUG(f'WiFi credentials not set... ssid: {ssid}, pw: {pw}')
+                    self._server.update_value(
+                        HubWifiService.ErrorCodeCharacteristic().uuid, BLEErrorCode.WIFI_CREDENTIAL_NOT_SET.value.to_bytes(2, 'little')
+                    )
+                    return
+                else:
+                    MXLOG_DEBUG('wifi credentials is set!', 'green')
+                    self._trigger.set()
+        except Exception as e:
+            MXLOG_DEBUG(f'Error occurred while writing characteristic: {e}', 'red')
+            self._server.update_value(HubWifiService.ErrorCodeCharacteristic().uuid, BLEErrorCode.FAIL.value.to_bytes(2, 'little'))
+
+    async def _add_service(self, service: Service):
+        await self._server.add_new_service(service.uuid)
+        for char in service.characteristics:
+            await self._server.add_new_characteristic(service.uuid, char.uuid, char.properties, char.value, char.permissions)
+
+    async def start(self):
+        MXLOG_DEBUG('Starting BLE advertiser...')
+        self._trigger.clear()
+        self._server = BlessServer(name=self._server_name)
+        self._server.read_request_func = self._read_request
+        self._server.write_request_func = self._write_request
+
+        await self._add_service(HubWifiService())
+
+        await self._server.start()
+        MXLOG_DEBUG('BLE Advertising started...')
+
+    async def is_advertising(self) -> bool:
+        if self._server is None:
+            return False
+
+        return await self._server.is_advertising()
+
+    async def wait_until_wifi_credentials_set(self, timeout: float = 30) -> Tuple[str, str, str, BLEErrorCode]:
+
+        async def wrapper() -> Tuple[str, str, str, BLEErrorCode]:
+            await self._trigger.wait()
+            ssid = self._server.get_characteristic(HubWifiService.SetWifiSSIDCharacteristic().uuid).value.decode()
+            pw = self._server.get_characteristic(HubWifiService.SetWifiPWCharacteristic().uuid).value.decode()
+            error_code = BLEErrorCode(int.from_bytes(self._server.get_characteristic(HubWifiService.ErrorCodeCharacteristic().uuid).value, 'little'))
+            return (ssid, pw, error_code)
+
+        try:
+            ssid, pw, error_code = await asyncio.wait_for(wrapper(), timeout)
+            return (ssid, pw, error_code)
+        except asyncio.TimeoutError:
+            return ('', '', '', BLEErrorCode.WIFI_CONNECT_TIMEOUT)
+
+    async def stop(self):
+        await self._server.stop()
+        MXLOG_DEBUG('BLE Advertising stopped...')
+
+    async def is_connected(self):
+        return await self._server.is_connected()
+
+
+if __name__ == '__main__':
+    START_LOGGER()
+
+    async def run():
+        ble_advertiser = BLEAdvertiser()
+        await ble_advertiser.start()
+        ssid, pw, error_code = await ble_advertiser.wait_until_wifi_credentials_set()
+        MXLOG_DEBUG(f'WiFi credentials set: ssid: {ssid}, pw: {pw}, error_code: {error_code}')
+        await ble_advertiser.stop()
+
+    async def test():
+        '''
+        NOTE: this test function should be run on a separate device
+        '''
+        from bleak import BleakClient, BleakScanner
+
+        server_name = "MySSIX BLE SERVER"
+        device_address = None
+
+        devices = await BleakScanner.discover()
+        for device in devices:
+            if device.name == server_name:
+                print(f'Found BLE server! name: {device.name}, address: {device.address}')
+                device_address = device.address
+                break
+        else:
+            print(f"Cannot find {server_name}")
+            return
+
+        ssid_characteristic_uuid = HubWifiService.SetWifiSSIDCharacteristic().uuid
+        pw_characteristic_uuid = HubWifiService.SetWifiPWCharacteristic().uuid
+        connect_wifi_characteristic_uuid = HubWifiService.ConnectWifiCharacteristic().uuid
+
+        ssid_value = b"ssid"
+        pw_value = b"password"
+
+        async with BleakClient(device_address) as client:
+            if client.is_connected:
+                print(f"Connected to {device_address}")
+
+                await client.write_gatt_char(ssid_characteristic_uuid, ssid_value)
+                print("WiFi SSID set")
+
+                await client.write_gatt_char(pw_characteristic_uuid, pw_value)
+                print("WiFi password set")
+
+                await client.write_gatt_char(connect_wifi_characteristic_uuid, bytearray([0x00]))
+                print("WiFi connection attempt")
+
+    asyncio.run(run())
